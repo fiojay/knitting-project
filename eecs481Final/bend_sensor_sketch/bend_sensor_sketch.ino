22e62422a4f7e26f0f95bc677b9c33c73246be69
astinlul@umich.edu <astinteferi@astins-MacBook-Air.local>
2014-12-12 11:53:18 -0500

    fin

--- eecs481Final/bend_sensor_sketch/bend_sensor_sketch.ino
+++ eecs481Final/bend_sensor_sketch/bend_sensor_sketch.ino
@@ -1,6 +1,8 @@
    
  int fsrPin = 0; // the FSR and 10K pulldown are connected to a0
  int fsrReading; // the analog reading from the FSR resistor divider
+ int sensorPin3 = A5;
+ int sensorValue3 = 0;
     void setup(void) {
    
       Serial.begin(9600);
@@ -9,18 +11,28 @@
     fsrReading = analogRead(fsrPin);
      // the raw analog reading
     // We'll have a few threshholds, qualitatively determined
-    if (fsrReading > 480) {
+    if (fsrReading > 80) {
       Serial.print("NoPressure ");
-      Serial.println(fsrReading);
+      Serial.println(fsrReading,DEC);
     } 
-    else if (fsrReading >= 330) {
+    else if (fsrReading >= 70) {
       Serial.print("AraTouch ");
-      Serial.println(fsrReading);
+      Serial.println(fsrReading, DEC);
     } 
-    else if (fsrReading < 330) {
+    else if (fsrReading < 50) {
       Serial.print("AraPushT ");
-      Serial.println(fsrReading);
+      Serial.println(fsrReading, DEC);
     } 
     
-    delay(1000);
+   delay(500);
+    
+   sensorValue3 = analogRead(sensorPin3);
+   if(sensorValue3 > 100) { 
+   Serial.print("AdultTouch "); 
+   Serial.println(sensorValue3, DEC);
+   delay(500);
+   }
+    
+    
+   
  } 
